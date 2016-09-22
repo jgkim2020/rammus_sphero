b5a4c618a7735115f3de44fd84f6ca7d4b811fc2
JiGang Kim <jgkim2020@snu.ac.kr>
2016-09-22 19:26:32 +0900

    Initial commit

--- lib/MPU6050/MPU6050.cpp
+++ lib/MPU6050/MPU6050.cpp
@@ -33,11 +33,14 @@ MPU6050::MPU6050() {
   temp_rawval = 0;
   gyro_rawval[0] = 0; gyro_rawval[1] = 0; gyro_rawval[2] = 0;
   acc_val[0] = 0.0; acc_val[1] = 0.0; acc_val[2] = 0.0;
+	acc_cval[0] = 0.0; acc_cval[1] = 0.0; acc_cval[2] = 0.0;
   temp_val = 0.0;
   gyro_val[0] = 0.0; gyro_val[1] = 0.0; gyro_val[2] = 0.0;
+	gyro_cval[0] = -3.0; gyro_cval[1] = -1.0; gyro_cval[2] = 1.0;
+	return;
 }
 
-void MPU6050::begin(void) {
+void MPU6050::begin() {
 	I2C i2c(I2C_SDA, I2C_SCL); // PB_9, PB_8
   int error = 0;
   data_write[0] = MPU6050_PWR;
@@ -48,49 +51,82 @@ void MPU6050::begin(void) {
   data_write[0] = MPU6050_GYRO_CONF;
   error += i2c.write(MPU6050_ADDR, data_write, 2, false)*100; // Gyroscope 131 LSB/dps
   error_msg(error, "MPU setup error");
+	return;
 }
 
-void MPU6050::read(void) {
+void MPU6050::read() {
 	I2C i2c(I2C_SDA, I2C_SCL); // PB_9, PB_8
   int error = 0;
   // Read accelerometer, gyroscope
   // i2c.read(int address, char *data, int length{#, bool repeated#})
   // i2c.write(int address, const char *data, int length{#, bool repeated#})
-  data_write[0] = ACC_XOUT_H;
-  error += i2c.write(MPU6050_ADDR, data_write, 1, true)*1;
-  error += i2c.read(MPU6050_ADDR, acc_read, 6, false)*10;
-  data_write[0] = TEMP_OUT_H;
-  error += i2c.write(MPU6050_ADDR, data_write, 1, true)*100;
-  error += i2c.read(MPU6050_ADDR, temp_read, 2, false)*1000;
-  data_write[0] = GYRO_XOUT_H;
-  error += i2c.write(MPU6050_ADDR, data_write, 1, true)*10000;
-  error += i2c.read(MPU6050_ADDR, gyro_read, 6, false)*100000;
+  this->data_write[0] = ACC_XOUT_H;
+  error += i2c.write(MPU6050_ADDR, this->data_write, 1, true)*1;
+  error += i2c.read(MPU6050_ADDR, this->acc_read, 6, false)*10;
+  this->data_write[0] = TEMP_OUT_H;
+  error += i2c.write(MPU6050_ADDR, this->data_write, 1, true)*100;
+  error += i2c.read(MPU6050_ADDR, this->temp_read, 2, false)*1000;
+  this->data_write[0] = GYRO_XOUT_H;
+  error += i2c.write(MPU6050_ADDR, this->data_write, 1, true)*10000;
+  error += i2c.read(MPU6050_ADDR, this->gyro_read, 6, false)*100000;
   // Merge MSB and LSB
-  acc_rawval[0] = (int16_t)((uint8_t)acc_read[0] << 8) | ((uint8_t)acc_read[1]);
-  acc_rawval[1] = (int16_t)((uint8_t)acc_read[2] << 8) | ((uint8_t)acc_read[3]);
-  acc_rawval[2] = (int16_t)((uint8_t)acc_read[4] << 8) | ((uint8_t)acc_read[5]);
-  temp_rawval = (int16_t)((uint8_t)temp_read[0] << 8) | ((uint8_t)temp_read[1]);
-  gyro_rawval[0] = (int16_t)((uint8_t)gyro_read[0] << 8) | ((uint8_t)gyro_read[1]);
-  gyro_rawval[1] = (int16_t)((uint8_t)gyro_read[2] << 8) | ((uint8_t)gyro_read[3]);
-  gyro_rawval[2] = (int16_t)((uint8_t)gyro_read[4] << 8) | ((uint8_t)gyro_read[5]);
+  this->acc_rawval[0] = (int16_t)((uint8_t)this->acc_read[0] << 8) | ((uint8_t)this->acc_read[1]);
+  this->acc_rawval[1] = (int16_t)((uint8_t)this->acc_read[2] << 8) | ((uint8_t)this->acc_read[3]);
+  this->acc_rawval[2] = (int16_t)((uint8_t)this->acc_read[4] << 8) | ((uint8_t)this->acc_read[5]);
+  this->temp_rawval = (int16_t)((uint8_t)this->temp_read[0] << 8) | ((uint8_t)this->temp_read[1]);
+  this->gyro_rawval[0] = (int16_t)((uint8_t)this->gyro_read[0] << 8) | ((uint8_t)this->gyro_read[1]);
+  this->gyro_rawval[1] = (int16_t)((uint8_t)this->gyro_read[2] << 8) | ((uint8_t)this->gyro_read[3]);
+  this->gyro_rawval[2] = (int16_t)((uint8_t)this->gyro_read[4] << 8) | ((uint8_t)this->gyro_read[5]);
   // Apply scale factor1
-  for(int i = 0; i < 3; i++) acc_val[i] = acc_rawval[i]/16384.0*9.8; // unit: m/s/s
-  temp_val = temp_rawval/340.0 + 36.53; // unit: degrees Celsius
-  for(int i = 0; i < 3; i++) gyro_val[i] = gyro_rawval[i]/131.0; // unit: rad/s
-  gyro_val[0] += 0.035;
-  gyro_val[1] += 0.030;
-  gyro_val[2] += 0.032;
-  error_msg(error, "MPU read error");
+  for(int i = 0; i < 3; i++) this->acc_val[i] = this->acc_rawval[i]/16384.0*9.8; // unit: m/s/s
+  this->temp_val = this->temp_rawval/340.0 + 36.53; // unit: degrees Celsius
+  for(int i = 0; i < 3; i++) this->gyro_val[i] = this->gyro_rawval[i]/131.0; // unit: rad/s
+  this->gyro_val[0] -= this->gyro_cval[0];
+  this->gyro_val[1] -= this->gyro_cval[1];
+  this->gyro_val[2] -= this->gyro_cval[2];
+  this->error_msg(error, "MPU read error");
+	return;
+}
+
+void MPU6050::calibrate() {
+	int i;
+	char option = 'g';
+	if(option == 'g')
+	{
+		this->gyro_cval[0] = 0;
+		this->gyro_cval[1] = 0;
+		this->gyro_cval[2] = 0;
+		for(i = 0; i < 100; i++)
+		{
+			this->read();
+			this->gyro_cval[0] += this->gyro_val[0];
+			this->gyro_cval[1] += this->gyro_val[1];
+			this->gyro_cval[2] += this->gyro_val[2];
+			wait_ms(10);
+		}
+		this->gyro_cval[0] *= 0.01;
+		this->gyro_cval[1] *= 0.01;
+		this->gyro_cval[2] *= 0.01;
+		return;
+	}
+	else if(option == 'a')
+	{
+		return;
+	}
+	else
+	{
+		return;
+	}
 }
 
 void MPU6050::error_msg(int error, const char *error_msg) {
 	DigitalOut myled(LED1);
 	Serial pc(SERIAL_TX, SERIAL_RX); // PA_2, PA_3
   if(error != 0) {
-    while(1) {
+  //  while(1) {
       myled = !myled;
       pc.printf("%s: %d\n", error_msg, error);
-      wait_ms(2500);
-    }
+    //}
   }
+	return;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,8 @@ Serial pc_serial(USBTX,USBRX);
 Serial pc(SERIAL_TX, SERIAL_RX); // PA_2, PA_3
 Serial_Receive blt(PC_12, PD_2);
 
-Ticker timer_int;
+Ticker sensor_tick;
+Ticker controller_tick;
 
 PwmOut motor1_control_pin_forward(PB_10);
 PwmOut motor1_control_pin_backward(PB_4);
@@ -34,95 +35,63 @@ PwmOut motor2_control_pin_backward(PB_5);
 Madgwick filter;
 MPU6050 sensor;
 
-float velocity_L = 0.0;
-float velocity_R = 0.0;
-float velocity_L_temp = 0.0;
-float velocity_R_temp = 0.0;
-float velocity_L_set = 0.0;
-float velocity_R_set = 0.0;
-float velocity_L_delta = 0.0;
-float velocity_R_delta = 0.0;
-float velocity_L_now = 0.0;
-float velocity_R_now = 0.0;
-
-int main_delay = 100;
-
-float target_velocity = 0.0f;
-
-bool newcommand = false;
-int tick = 0;
-int debug_cnt = 0;
+float Roll;
+float Pitch;
+float Yaw;
+float setYaw = 0;
+bool command = false;
 
 void initialize(void);
 void motor_control(float velocity, int motor_num);
 
-void timer_int_handler(void)
+void sensor_tick_handler(void)
 {
-  //pc_serial.printf("%d\n", debug_cnt);
-  //debug_cnt++;
-  if(newcommand == true){
-    tick = 200;
-    newcommand = false;
-  }
-  else if((newcommand == false) && (tick > 0)) {
-    tick--;
-    velocity_L_now += velocity_L_delta/200.0;
-    velocity_R_now += velocity_R_delta/200.0;
-    motor_control(velocity_L_now, 1);
-    motor_control(velocity_R_now, 2);
-  }
-  if(tick == 0) {
-    motor_control(velocity_L_set, 1);
-    motor_control(velocity_R_set, 2);
+  sensor.read();
+  filter.updateIMU(sensor.gx(), sensor.gy(), sensor.gz(), sensor.ax(), sensor.ay(), sensor.az());
+  Roll = filter.getRoll();
+  Pitch = filter.getPitch();
+  Yaw = filter.getYaw();
+  return;
+}
+
+void controller_tick_handler(void)
+{
+  if(command == false) return;
+  else
+  {
+    return;
   }
 }
 
-int main(void){
+int main(void)
+{
   initialize();
-  while(1) {
-    //blt.Motor_reset();
-    velocity_L_temp = velocity_L;
-    velocity_R_temp = velocity_R;
-    velocity_L = blt.Get_processed_motor_Value('L');
-    //pc_serial.printf("L : %f\n",velocity_L);
-    velocity_R = blt.Get_processed_motor_Value('R');
-    //pc_serial.printf("R : %f\n",velocity_R);
-    if((velocity_L != velocity_L_temp) || (velocity_R != velocity_R_temp)){
-      if((velocity_L == 0) && (velocity_R == 0)){
-        velocity_L_set = 0.0;
-        velocity_R_set = 0.0;
-        velocity_L_now = velocity_L_set;
-        velocity_R_now = velocity_L_set;
-        newcommand = false;
-        tick = 0;
-      }
-      else{
-        velocity_L_set = velocity_L;
-        velocity_R_set = velocity_R;
-        velocity_L_delta = velocity_L_set - velocity_L_now;
-        velocity_R_delta = velocity_R_set - velocity_R_now;
-        newcommand = true;
-      }
-    }
-    //motor_control(velocity_L,1);
-    //motor_control(velocity_R,2);
-    //wait_ms(main_delay);
+  while(1)
+  {
+    pc.printf("%f %f %f\n", Roll, Pitch, Yaw);
   }
+  return 0;
 }
 
-void initialize(){
-  pc_serial.baud(115200);
-  timer_int.attach(&timer_int_handler, 0.01);
-  blt.baud(115200);
-  blt.bluetooth.attach(&blt, &Serial_Receive::Receive_data, Serial::RxIrq);
+void initialize()
+{
   motor1_control_pin_forward.period_us(400);
   motor1_control_pin_backward.period_us(400);
   motor2_control_pin_forward.period_us(400);
   motor2_control_pin_backward.period_us(400);
+  pc_serial.baud(115200);
+  blt.baud(115200);
+  blt.bluetooth.attach(&blt, &Serial_Receive::Receive_data, Serial::RxIrq);
+  sensor.begin();
+  //sensor.calibrate();
+  filter.begin(100);
+  sensor_tick.attach(&sensor_tick_handler, 0.01);
+  controller_tick.attach(&controller_tick_handler, 0.05);
   return;
 }
 
-void motor_control(float velocity, int motor_num){
+void motor_control(float velocity, int motor_num)
+{
   if(motor_num==1){
     if(velocity>=0){
       motor1_control_pin_forward.write(velocity);
@@ -145,89 +114,3 @@ void motor_control(float velocity, int motor_num){
   }
   return;
 }
-
-//copy end
-
-/*
-//LED FOR DEBUG
-
-
-//set pin number
-Serial pc(USBTX, USBRX); // tx, rx
-
-PwmOut motor_control_pin(D2);
-
-int encoder_val[2] = {0,0};
-float encoder_velocity;
-float encoder_velocity_error[3] = {0,0,0};
-
-char command = 'd';
-
-float target_velocity = 0.0f;
-
-float motor_input = 0.0f;
-
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////VARIABLES///////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-float Kp = 0.0f;
-float Ki = 0.0f;
-float Kd = 0.0f;
-
-int main_delay = 250;
-////////////////////////////////////////////////////////////////////////////////
-
-void pc_control(){
-  if(pc.readable()){
-    command = pc.getc();
-  }
-}
-
-void encoder_read(){
-  int tmp = encoder1.getCurrentState();
-  encoder_val[0] = encoder_val[1];
-  encoder_val[1] = tmp;
-  encoder_velocity = encoder_val[1] - encoder_val[0];
-  encoder_velocity_error[0] = encoder_velocity_error[1];
-  encoder_velocity_error[1] = encoder_velocity_error[2];
-  encoder_velocity_error[2] = target_velocity - encoder_velocity;
-}
-
-void motor_control(float velocity){
-  float motor_input_mod = Kp*(encoder_velocity_error[2]-encoder_velocity_error[1])
-                          + Ki*(encoder_velocity_error[2])
-                          + Kd*((encoder_velocity_error[2]-encoder_velocity_error[1])-(encoder_velocity_error[1]-encoder_velocity_error[0]));
-  motor_input += motor_input_mod;
-  motor_control_pin.write(motor_input);
-
-}
-
-void initialize(){
-  motor_control_pin.period_us(400);
-}
-
-int main(void){
-  initialize();
-  while(1) {
-    encoder_read();
-    pc_control();
-    switch (command) {
-      case 'w':
-      target_velocity = forward_velocity;
-        break;
-      case 's':
-      target_velocity = backward_velocity;
-        break;
-      default:
-      target_velocity = 0.0f;
-        break;
-    }
-    motor_control(target_velocity);
-    encoder1.reset();
-    wait_ms(main_delay);
-  }
-}
-
-
-
-*/
